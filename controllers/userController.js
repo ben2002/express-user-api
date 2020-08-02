c8dad8556fe6adf483fcec4c949aa5bd0cf0f377
Ben Moser <benjamin.moser@hs-augsburg.de>
2020-08-02 23:37:29 +0200

    add CRUD operations

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,60 @@
 const userModel = require('../models/userModel');
+const { User } = require('../models/userModel');
 
-const getUser = (req, res) => {
-	console.log('test user get route');
+// @route   GET api/user
+// @desc    get all users
+const getUsers = async (req, res) => {
+	try {
+		const allUsers = await User.find();
+		res.json(allUsers);
+	} catch (error) {
+		console.log('Error:', error);
+	}
 };
 
-const createUser = (req, res) => {
+// @route   GET api/user
+// @desc    get one user
+const getUser = async (req, res) => {
+	const _id = req.params.id;
+	try {
+		const user = await User.findById(_id);
+		if (!user) res.status(404).send('The user with the given ID was not found');
+		res.json(user);
+	} catch (error) {
+		console.log('Error:', error);
+	}
+};
+
+// @route   POST api/user
+// @desc    create new user
+const createUser = async (req, res) => {
 	try {
 		console.log('test user post route');
 		console.log(req.body);
 
 		const user = new userModel.User(req.body);
-		const savedUser = user.save();
+		const savedUser = await user.save();
 
 		console.log(savedUser);
 
 		res.json(savedUser);
 	} catch (error) {
-		console.log(error);
+		console.log('Error:', error);
 	}
 };
 
+// @route   PUT api/user
+// @desc    update a user
+const updateUser = async (req, res) => {};
+
+// @route   DELETE api/user
+// @desc    delete a user
+const deleteUser = async (req, res) => {};
+
 module.exports = {
+	getUsers,
 	getUser,
-	createUser
+	createUser,
+	updateUser,
+	deleteUser
 };
--- routes/v1/user.js
+++ routes/v1/user.js
@@ -4,13 +4,28 @@ const router = express.Router();
 const userController = require('../../controllers/userController');
 
 // @route   GET api/user
-// @desc    Test get route
-// @access  Public
-router.get('/', userController.getUser);
+// @desc    Test get all users route
+// @access  Admin
+router.get('/', userController.getUsers);
+
+// @route   GET api/user
+// @desc    Test get one user route
+// @access  Private
+router.get('/:id', userController.getUser);
 
 // @route   POST api/user
 // @desc    Test post route
 // @access  Public
 router.post('/', userController.createUser);
+
+// @route   PUT api/user
+// @desc    Test update route
+// @access  Private
+router.put('/', userController.updateUser);
+
+// @route   DELETE api/user
+// @desc    Test delete route
+// @access  Private
+router.delete('/', userController.deleteUser);
 
 module.exports = router;
